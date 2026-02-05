935eaf715c900d82ce7176d678a613e33a4d6b10
Lebo Madikane <madikanelebohang@gmail.com>
2026-02-05 12:36:03 +0200

    Set up Header and Hero components

--- src/app/page.js
+++ src/app/page.js
@@ -1,1 +1,13 @@
+import Header from "@/components/Header/Header"
+import Hero from "@/components/Hero/Hero"
 
+export default function HomePage() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Hero />
+            </main>
+        </>
+    )
+}
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import styles from './Hero.module.scss'
+
+export default function Hero() {
+    return (
+        <header className={styles.hero}>
+            <div className={styles.container}>
+
+            </div>
+        </header>
+    )
+}
