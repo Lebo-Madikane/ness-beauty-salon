68f8c8678acdfc55b8a4183a456c37ab432dba88
Lebo Madikane <madikanelebohang@gmail.com>
2026-02-05 20:05:44 +0200

    structure Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,23 @@
+'use client'
+import { useState } from 'react'
+import Link from 'next/link'
 import styles from './Header.module.scss'
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
     return (
         <header className={styles.header}>
             <div className={styles.container}>
                 <div className={styles.logo}>LOGO</div>
                 <div className={styles.nav}>
                     <p>Home</p>
-                    <p>Services</p>
-                    <p>Contact</p>
+                    <p className={styles.desktopNav}>Services</p>
+                    <p className={styles.desktopNav}>Contact</p>
                     <div>Book Now</div>
                 </div>
             </div>
