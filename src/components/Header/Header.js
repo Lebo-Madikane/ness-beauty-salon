4e1dfd525b816427854e19384c530dc5fb1dc3d6
Lebo Madikane <madikanelebohang@gmail.com>
2026-02-05 12:42:17 +0200

    Set up Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,13 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
-                    
+                <div className={styles.logo}>LOGO</div>
+                <div className={styles.nav}>
+                    <p>Home</p>
+                    <p>Services</p>
+                    <p>Contact</p>
+                    <div>Book Now</div>
+                </div>
             </div>
         </header>
     )
