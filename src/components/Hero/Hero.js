bf82b5e0331c5e740d056959f13e8280be21f193
Lebo Madikane <madikanelebohang@gmail.com>
2026-02-07 21:28:07 +0200

    structure Hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,9 @@ export default function Hero() {
     return (
         <header className={styles.hero}>
             <div className={styles.container}>
-
+                <div className={styles.top}></div>
+                <div className={styles.mid}></div>
+                <div className={styles.bottom}></div>
             </div>
         </header>
     )
