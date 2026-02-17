a77c5d226bf69860932dbbc043ce3e2fac4a923c
Lebo Madikane <madikanelebohang@gmail.com>
2026-02-17 22:05:09 +0200

    built component structure

--- src/components/Sections/Hero/Hero.js
+++ src/components/Sections/Hero/Hero.js
@@ -1,11 +1,16 @@
 import styles from './Hero.module.scss'
+import Image from 'next/image'
 
 export default function Hero() {
     return (
         <section className={styles.hero}>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    <h3>HERO</h3>
+                    <h1 className={styles.structure}>NESS SALON</h1>
+                    <Image className={styles.structure} src='/images/hero-images/hero-main.webp' alt="Hero Image" width={150} height={100} priority={true} />
+                    <Image className={styles.structure} src='/images/hero-images/hair-cropped.webp' alt="Hero Image" width={150} height={100} priority={true} />
+                    <h2 className={styles.structure}>BEAUTY</h2>
+                    <Image className={styles.structure} src='/images/hero-images/hair-full.webp' alt="Hero Image" width={150} height={100} priority={true} />
                 </div>
             </div>
         </section>
