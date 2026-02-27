555b4d6c377db1721de4d7c0971246f470c9a28a
Lebo Madikane <madikanelebohang@gmail.com>
2026-02-27 14:46:56 +0200

    build booking form

--- src/components/Sections/Form/Form.js
+++ src/components/Sections/Form/Form.js
@@ -1,11 +1,32 @@
 import styles from './Form.module.scss'
+import Card from '@/components/UI/Card/Card'
+import Button from '@/components/UI/Button/Button'
 
 export default function Form() {
     return (
-        <section className={styles.form}>
+        <section className={styles.formSection}>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    <h3>FORM</h3>
+                    <Card className={styles.formCard}>
+                        <form className={styles.form}>
+                            <h3>Reserve Your Time</h3>
+                            <p>Select your preferred date and time</p>
+                            <div className={styles.field}>
+                                <input placeholder='Name'/>
+                                <input placeholder='Last Name' />
+                            </div>
+                            <div className={styles.field}>
+                                <input placeholder='+2784 391 7945' />
+                                <label>Select Service</label>
+                                <select></select>
+                            </div>
+                            <div className={styles.field}>
+                                <input type='date'/>
+                                <input type='time'/>
+                            </div>
+                            <Button>SUBMIT</Button>
+                        </form>
+                    </Card>
                 </div>
             </div>
         </section>
