1e53674ea0f68572cb726f0c2bd1a73a619e800a
Lebo Madikane <madikanelebohang@gmail.com>
2026-03-02 11:53:05 +0200

    structure Testimonial Component

--- src/components/Sections/Testimonial/Testimonial.js
+++ src/components/Sections/Testimonial/Testimonial.js
@@ -5,7 +5,20 @@ export default function Testimonial() {
         <section className={styles.testimonial}>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    <h3>TESTIMONIAL</h3>
+                    <div className={styles.testimony}>
+                        <h2><i>"</i></h2>
+                        <p>The atmosphere at Ness Beauty Salon is as refined as the style.<br/>
+                            I’ve never felt so understood by a stylist.  Truly an artistic experience.</p>
+                        <span></span>
+                        <h6>ELENA VANCE</h6>
+                        <p>Loyal Customer Since 2017</p>
+                    </div>
+                    <div className={styles.location}>
+                        <p className='address'> 100 Plain Street, Cape Town</p>
+                    </div>
+                    <div className={styles.contact}>
+                        <p>Call: 076 380 0939<br />Email: nosi_pure@yahoo.com</p>
+                    </div>
                 </div>
             </div>
         </section>
