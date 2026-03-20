fd667ce3b806ea9b42f85d88f67173b7ab12f7b0
Lebo Madikane <madikanelebohang@gmail.com>
2026-03-20 12:33:47 +0200

    build galleryPage functionality

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -11,6 +11,10 @@ export default function GalleryPage() {
             <Header />
             <Cta />
             <main className={styles.container}>
+                <div className={styles.aboutOne}>
+                    <h4>Pure Style</h4>
+                    <p>Crafted from nature's best, every style is a blend of pure ingridients and vibrant product.</p>
+                </div>
                 <div className={styles.gallery}>
                     <div className={styles.row}>
                         <div className={styles.galleryOne}></div>
@@ -19,6 +23,14 @@ export default function GalleryPage() {
                     <div className={styles.row}>
                         <div className={styles.galleryThree}></div>
                         <div className={styles.galleryFour}></div>
+                    </div>
+                </div>
+                <div className={styles.aboutTwo}>
+                    <p>Boys Cut: Special</p>
+                    <div className={styles.circleContainer}>
+                        <div className={styles.circle}></div>
+                        <div className={styles.circle}></div>
+                        <div className={styles.circle}></div>
                     </div>
                 </div>
             </main>
--- src/components/Layout/CTA/Cta.js
+++ src/components/Layout/CTA/Cta.js
@@ -8,8 +8,8 @@ export default function Cta() {
         <section className={styles.cta}>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    <Link href={"#services"}><Button >Services</Button></Link>
-                    <Link href={"#contact"}><Button variant='secondary'>Contact</Button></Link>
+                    <Link href={"/#services"}><Button >Services</Button></Link>
+                    <Link href={"/#contact"}><Button variant='secondary'>Contact</Button></Link>
                 </div>
             </div>
         </section>
