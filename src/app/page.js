a038a9055496db7aa1cb9b91a82469f8a5358bc3
Lebo Madikane <madikanelebohang@gmail.com>
2026-04-09 20:49:42 +0200

    add homepage scss for background img

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import styles from "./page.module.scss"
 import Header from "@/components/Layout/Header/Header"
 import Cta from "@/components/Layout/CTA/Cta"
 import ScrollSections from "@/components/Layout/ScrollSections/ScrollSections"
@@ -7,13 +8,13 @@ import Footer from "@/components/Layout/Footer/Footer"
 
 export default function HomePage() {
     return (
-        <>
+        <div className={styles.homepage}>
             <Header />
             <main>
                 <Cta />
                 <ScrollSections />
                 <Footer />
             </main>
-        </>
+        </div>
     )
 }
