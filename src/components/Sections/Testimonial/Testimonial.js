f949d86c8ff06372d06245375e303708bd2c4b9e
Lebo Madikane <madikanelebohang@gmail.com>
2026-04-14 21:32:23 +0200

    style Testimonial component

--- src/components/Sections/Testimonial/Testimonial.js
+++ src/components/Sections/Testimonial/Testimonial.js
@@ -1,4 +1,5 @@
 import styles from './Testimonial.module.scss'
+import Image from 'next/image'
 
 export default function Testimonial() {
     return (
@@ -9,8 +10,8 @@ export default function Testimonial() {
                         <div className={styles.qoute}>"</div>
                         <p>The atmosphere at Ness Beauty Salon is as refined as the style.
                             I’ve never felt so understood by a stylist.  Truly an artistic experience.</p>
-                        <span></span>
-                        <h6>ELENA VANCE</h6>
+                        <Image className={styles.span} src='/images/services-images/Braides.webp' alt="Hero Image" width={150} height={100} priority={true} />
+                        <h5>ELENA VANCE</h5>
                         <p>Loyal Customer Since 2017</p>
                     </div>
                     <div className={styles.location}>
