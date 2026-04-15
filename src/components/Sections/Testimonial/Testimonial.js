84b7b483219d1075cf3f2b34c5c0fd4600d8698c
Lebo Madikane <madikanelebohang@gmail.com>
2026-04-15 19:44:26 +0200

    align content

--- src/components/Sections/Testimonial/Testimonial.js
+++ src/components/Sections/Testimonial/Testimonial.js
@@ -8,14 +8,13 @@ export default function Testimonial() {
                 <div className={styles.content}>
                     <div className={styles.testimony}>
                         <div className={styles.qoute}>"</div>
-                        <p>The atmosphere at Ness Beauty Salon is as refined as the style.
-                            I’ve never felt so understood by a stylist.  Truly an artistic experience.</p>
+                        <p>I’ve never felt so understood by a stylist.  Truly an artistic experience.</p>
                         <Image className={styles.span} src='/images/services-images/Braides.webp' alt="Testimony Image" width={150} height={100} priority={true} />
                         <h5>ELENA VANCE</h5>
                         <p>Loyal Customer Since 2017</p>
                     </div>
                     <div className={styles.location}>
-                        <p className='address'> 100 Plain Street, Cape Town</p>
+                        <p className={styles.address}> 100 Plain Street, Cape Town</p>
                     </div>
                     <div className={styles.contact}>
                         <p>Call: 076 380 0939<br />Email: nosi_pure@yahoo.com</p>
